aa06927684d6cb8c5762dd08ac2e6c0518b09762
riazonbin <riazonbin@mail.ru>
2023-04-07 21:53:10 +0300

    Add project files.

---
+++ LanguageSchool/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using LanguageSchool.Components;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LanguageSchool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static readonly LanguageSchoolEntities Connection = new LanguageSchoolEntities();
+
+        public static bool IsAdminMode = false;
+
+        public static Visibility IsAdminModeVisible = IsAdminMode ? Visibility.Visible : Visibility.Hidden;
+    }
+}
---
+++ LanguageSchool/Components/ExtensionClasses/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LanguageSchool.Components
+{
+    public partial class Service
+    {
+        public Visibility IsHaveDiscount => Discount > 0 ? Visibility.Visible : Visibility.Collapsed;
+        public decimal CostWithDiscount => Discount > 0 ? Cost - Cost * (decimal)Discount / 100 : Cost;
+        public string DurationInMinutes => DurationInSeconds == 0 ? "" : $"{DurationInSeconds / 60} минут";
+        public string DiscountDisplay => Discount == null || Discount == 0 ? "" : $"* скидка {Discount}%";
+        public string ImagePath => $@"/{MainImagePath}";
+        public Visibility AdminVisibility => App.IsAdminMode ? Visibility.Visible : Visibility.Collapsed;
+    }
+}
