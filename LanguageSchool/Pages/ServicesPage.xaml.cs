b5a6b70b2e80e30ed7c5d90cff5aa537205a7d68
riazonbin <riazonbin@mail.ru>
2023-04-08 13:48:35 +0300

    added filtration with predicate and function

--- LanguageSchool/Pages/ServicesPage.xaml.cs
+++ LanguageSchool/Pages/ServicesPage.xaml.cs
@@ -25,6 +25,10 @@ namespace LanguageSchool.Pages
 
         public List<Service> ModifiedServices { get; set; }
 
+        private Predicate<Service> _filterQuery = x => true;
+
+        private Func<Service, object> _sortQuery = x => x.ID;
+
         public ServicesPage()
         {
             InitializeComponent();
@@ -37,16 +41,39 @@ namespace LanguageSchool.Pages
 
         private void tbSearchNameTextChanged(object sender, TextChangedEventArgs e)
         {
-            if(tbSearchName.Text == "00000")
+            SearchServicesByName();
+        }
+
+        private void SearchServicesByName()
+        {
+            if(lvServices == null)
+            {
+                return;
+            }
+
+            if (tbSearchName.Text == "00000")
             {
                 App.IsAdminMode = true;
             }
 
-            ModifiedServices = AllServices.Where(x => x.Title.ToLower().Contains(tbSearchName.Text.ToLower())).ToList();
+            lvServices.ItemsSource = ModifiedServices.Where(x => x.Title.ToLower().Contains(tbSearchName.Text.ToLower())).ToList();
+        }
 
-            lvServices.ItemsSource = ModifiedServices;
-            SortPrice();
+        private void UpdateData()
+        {
+            if(lvServices == null)
+            {
+                return;
+            }
 
+           ModifiedServices = AllServices 
+                .Where(
+                x => _filterQuery(x))
+                .OrderBy(x => _sortQuery(x))
+                .ToList();
+
+            SearchServicesByName();
+            
         }
 
         private void cbSortPriceSelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -67,17 +94,63 @@ namespace LanguageSchool.Pages
             switch (selectedSortText)
             {
                 case "По возрастанию":
-                    lvServices.ItemsSource = ModifiedServices.OrderBy(x => x.CostWithDiscount);
+                    _sortQuery = x => x.CostWithDiscount;
                     break;
 
                 case "По убыванию":
-                    lvServices.ItemsSource = ModifiedServices.OrderByDescending(x => x.CostWithDiscount);
+                    _sortQuery = x => -x.CostWithDiscount;
                     break;
 
                 default:
-                    lvServices.ItemsSource = ModifiedServices;
+                    _sortQuery = x => x.ID;
                     break;
             }
+
+            UpdateData();
+        }
+
+        private void FiltrateDiscount()
+        {
+            if(lvServices == null)
+            {
+                return;
+            }
+
+            ComboBoxItem item = (ComboBoxItem)cbFilterDiscount.SelectedItem;
+            string selectedSortText = item.Content.ToString();
+
+            switch (selectedSortText)
+            {
+                case "от 0 до 5%":
+                    _filterQuery = x => x.Discount >= 0 && x.Discount < 5;
+                    break;
+
+                case "от 5 до 15%":
+                    _filterQuery = x => x.Discount >= 5 && x.Discount < 15;
+                    break;
+
+                case "от 15 до 30%":
+                    _filterQuery = x => x.Discount >= 15 && x.Discount < 30;
+                    break;
+
+                case "от 30 до 70%":
+                    _filterQuery = x => x.Discount >= 30 && x.Discount < 70;
+                    break;
+
+                case "от 70 до 100%":
+                    _filterQuery = x => x.Discount >= 70 && x.Discount < 100;
+                    break;
+
+                default:
+                    break;
+            }
+
+            UpdateData();
+        }
+
+        private void cbFilterDiscountSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FiltrateDiscount();
         }
     }
 }
